1b578d0998408ec6a27954ca877d965cf55e6182
Evangelos Giataganas <e.giataganas@gmail.com>
2022-08-24 19:31:08 +0300

    Add address delate on planning_application

--- app/models/planning_application.rb
+++ app/models/planning_application.rb
@@ -2,5 +2,7 @@ class PlanningApplication < ApplicationRecord
   belongs_to :local_authority
   belongs_to :property
 
+  delegate :address, to: :property
+
   validates :reference, :area, :proposal, :received_at, :decision, :decision_issued_at, :local_authority, :property, presence: true
 end
