469dd98d6fe75018bc5fe0100d6bb16807325363
Evangelos Giataganas <e.giataganas@gmail.com>
2022-10-17 16:08:35 +0300

    Add a wrapper class of jwt gem

---
+++ app/models/json_web_token.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class JsonWebToken
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  class << self
+    def encode(payload)
+      JWT.encode(payload, SECRET_KEY)
+    end
+
+    def decode(token)
+      decoded = JWT.decode(token, SECRET_KEY)[0]
+      HashWithIndifferentAccess.new(decoded)
+    end
+  end
+end
