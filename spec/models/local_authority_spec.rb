162d8b1a9e1d6091dae8d9b07d02dffc60d2b6d8
Evangelos Giataganas <e.giataganas@gmail.com>
2022-10-21 15:04:27 +0300

    Add an enum table for local_authority name

--- spec/models/local_authority_spec.rb
+++ spec/models/local_authority_spec.rb
@@ -3,6 +3,12 @@
 require "rails_helper"
 
 RSpec.describe LocalAuthority, type: :model do
+  it "raises an error with wrong name" do
+    expect { build(:local_authority, name: "local_authority") }
+      .to raise_error(ArgumentError)
+      .with_message(/is not a valid name/)
+  end
+
   context "when validations are run" do
     it "must have the name set" do
       local_authority = build(:local_authority, name: nil)
