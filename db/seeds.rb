8ab0ad2ebdcbedc817f401e625c22e324025ced3
Evangelos Giataganas <e.giataganas@gmail.com>
2022-10-28 16:07:34 +0300

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
-
 # Local authority
 
 ["buckinghamshire", "lambeth", "southwark"].each do |local_authority|
-  LocalAuthority.find_or_create_by(name: local_authority)
+  LocalAuthority.find_or_create_by!(name: local_authority)
 end
