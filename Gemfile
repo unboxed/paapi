52892c2e0f7d94f85b6f373dbc1e59771831acf2
Kevin Sedgley <kevin.sedgley@unboxedconsulting.com>
2023-01-31 10:28:37 +0000

    WIP

--- Gemfile
+++ Gemfile
@@ -22,8 +22,8 @@ gem "sassc-rails"
 gem "sidekiq"
 gem "sprockets-rails"
 gem "stimulus-rails"
-gem "turbo-rails"
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
+gem "devise" 
 
 group :development, :test do
   gem "brakeman", require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   mount Rswag::Ui::Engine => "/api-docs"
 
   get :healthcheck, to: proc { [200, {}, %w[OK]] }
@@ -10,4 +11,6 @@ Rails.application.routes.draw do
       resources :planning_applications, only: %i[index create]
     end
   end
+
+  root to: 'home#index'
 end
