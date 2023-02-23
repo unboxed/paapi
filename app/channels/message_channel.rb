e06d534f551b8cacaa3786f710ea8a9fa3a148c8
Kevin Sedgley <kevin.sedgley@unboxedconsulting.com>
2023-02-23 20:03:40 +0000

    Live messages

---
+++ app/channels/message_channel.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class MessageChannel < ApplicationCable::Channel
+  def subscribed
+    subscription_string = "message_channel#{params.key?('path') ? params['path'] : ''}"
+    stream_from subscription_string
+    Rails.logger.info("Set stream_from to #{subscription_string}")
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data); end
+end
