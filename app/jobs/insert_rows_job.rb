19e27d25fd81459a6e8d2092523c29f3bc7200f8
Kevin Sedgley <kevin.sedgley@unboxedconsulting.com>
2023-03-07 10:49:00 +0000

    Insert rows

--- app/jobs/insert_rows_job.rb
+++ app/jobs/insert_rows_job.rb
@@ -1,65 +1,44 @@
 # frozen_string_literal: true
 
+require "csv"
+
 class InsertRowsJob < ApplicationJob
   def perform(csv_upload)
     @csv_upload = csv_upload
-    add_message "Starting CSV Upload"
+    add_message "Starting Insert Rows"
 
-    if csv_has_errors?
-      add_message "Job stopped - errors in import"
-    else
-      add_message "No errors - now inserting rows"
-      InsertRowsJob.perform_later @csv_upload
-    end
+    insert_from_all_files
   end
 
-  def csv_has_errors?
+  def insert_from_all_files
     results = []
     has_errors = false
     @csv_upload.csv_files.each do |csv_file|
       csv_file.open do |csv_file_info|
-        add_message "Checking rows for #{csv_file.filename}"
-        csv_results = check_rows(csv_file_info)
-        if csv_results[0]
-          has_errors = true
-        else
-          results.push [csv_results]
-        end
+        add_message "Inserting rows for #{csv_file.filename}"
+        insert_rows(csv_file_info)
       end
     end
-    has_errors
+
+    add_message "Completed adding rows"
   end
 
-  def check_rows(file_info)
+  def insert_rows(file_info)
     results = []
-    error_occured = false
     CSV.foreach(file_info.path, headers: true, header_converters: :symbol) do |row|
-      error = check_row_for_errors(row)
-
-      if error
-        results.push error
-        error_occured = true
-        add_message "Error in row #{results.count} - #{error}", :error
-      else
-        add_message "No errors in row #{results.count}"
-        results.push true
-      end
+      insert_row(row)
+      results.push true
+      add_message "Inserted row #{results.count}", :info 
+      sleep 0.01
     end
 
     file_info.unlink
-    [error_occured, results]
   end
 
-  def check_row_for_errors(row)
-    planning_application = PlanningApplicationCreation.new(
+  def insert_row(row)
+    planning_app_creation = PlanningApplicationCreation.new(
       **row.to_h.merge(local_authority:)
-    )
-    begin
-      planning_application.validate
-      false
-    rescue StandardError => e
-      e.message
-    end
+    ).perform
   end
 
   def local_authority
