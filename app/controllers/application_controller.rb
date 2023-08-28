cc11a2dcb40655a6286ef917c82559b0ff577c73
Evangelos Giataganas <e.giataganas@gmail.com>
2023-08-28 16:46:13 +0300

    Set current user and local_authority

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_current_user
+  before_action :set_current_local_authority
+
+  attr_reader :current_local_authority
+
+  helper_method :current_local_authority
+
+  private
+
+  def set_current_user
+    Current.user = current_user
+  end
+
+  def set_current_local_authority
+    @current_local_authority ||= current_user&.local_authority if current_user
+  end
 end
